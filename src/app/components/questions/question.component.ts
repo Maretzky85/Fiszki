59d23e45339b26734d329a385dbdcb10214ae02d
maretzky85 <maretzky85@gmail.com>
2019-06-25 13:34:13 +0200

    security audit

--- src/app/components/questions/question.component.ts
+++ src/app/components/questions/question.component.ts
@@ -30,6 +30,9 @@ export class QuestionComponent implements OnInit {
 
   maxPage;
 
+  private swipeCoord?: [number, number];
+  private swipeTime?: number;
+
   constructor(private connection: ConnectionService,
               private dataSharing: DataSharingService,
               private notify: NotificationService,
@@ -108,5 +111,36 @@ export class QuestionComponent implements OnInit {
       this.params.page = this.params.page - 1;
     }
     this.loadAllQuestions();
+  }
+
+  swipe(e: TouchEvent, when: string): void {
+    const coord: [number, number] = [e.changedTouches[0].clientX, e.changedTouches[0].clientY];
+    const time = new Date().getTime();
+
+    if (when === 'start') {
+      this.swipeCoord = coord;
+      this.swipeTime = time;
+    } else if (when === 'end') {
+      const direction = [coord[0] - this.swipeCoord[0], coord[1] - this.swipeCoord[1]];
+      const duration = time - this.swipeTime;
+
+      if (duration < 1000 //
+        && Math.abs(direction[0]) > 30 // Long enough
+        && Math.abs(direction[0]) > Math.abs(direction[1] * 3)) { // Horizontal enough
+        const swipe = direction[0] < 0 ? 'next' : 'previous';
+        if (swipe === 'next') {
+          if (this.admin) {
+            this.loadAllNext();
+          } else {
+            this.loadNext();
+          }
+        }
+        if (swipe === 'previous') {
+          if (this.admin) {
+            this.loadAllPrev();
+          }
+        }
+      }
+    }
   }
 }
