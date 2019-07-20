9417e7f2fc2f1a25629f76c86647251a1761c102
maretzky85 <maretzky85@gmail.com>
2019-07-21 00:59:46 +0200

    added question reference in admin page while viewing answers

--- src/app/models/answerModel.ts
+++ src/app/models/answerModel.ts
@@ -3,5 +3,6 @@ import {Answer} from './responseInterface';
 export class AnswerModel implements Answer {
   answer: string;
   id: number;
-  user: String;
+  user: string;
+  question_id: number;
 }
